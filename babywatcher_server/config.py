963d459723b7a8fbc86a86e6157b27370d02d8cf
Kim Desrosiers <kimdesro@gmail.com>
2018-03-04 22:07:57 -0500

    HUge refactor. Remove the awkward implementation using a custom daemon system. Use pipenv instead of a setup.py. Use flask as web framework. Use opencv to interact with the camera.

--- babywatcher_server/config.py
+++
@@ -1,6 +0,0 @@
-class Config(object):
-    def __init__(self):
-        self.read_configs()
-
-    def read_configs(self):
-        pass
--- babywatcher_server/server.py
+++ babywatcher_server/server.py
@@ -1,26 +1,36 @@
-import socketserver
-from babywatcher_server.daemon import Daemon
-from babywatcher_server.stream import Stream
 
 # server constant
+from flask import Flask, render_template, Response
+
+from babywatcher_server.camera import OpenCvCamera
+
 HOST = '0.0.0.0'
 PORT = 5050
-ADDR = (HOST, PORT)
-BUFSIZE = 4096
 
 
-class TCPHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        self.data = self.request.recv(1024).strip()
-        if self.data == "getStreamUrl":
-            stream_url = Stream.getStream()
-            self.request.sendall(stream_url)
-        elif self.data == "stopStream":
-            Stream.stopStream()
-            self.request.sendall("StreamClosed")
+app = Flask(__name__)
 
 
-class BabyWatcherServer(Daemon):
-    def run(self):
-        serv = socketserver.TCPServer((HOST, PORT), TCPHandler)
-        serv.serve_forever()
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('template/index.html')
+
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(OpenCvCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host=HOST, threaded=True)
